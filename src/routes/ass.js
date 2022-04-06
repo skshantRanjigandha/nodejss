3db238a7433bea1335557c459b8c18c929cf259c
Sushant_KB-uranium <102942750+skshantRanjigandha@users.noreply.github.com>
2022-04-06 22:18:19 +0530

    added new

---
+++ src/routes/ass.js
@@ -0,0 +1,7 @@
+let a = "https://www.google.com"
+
+module.exports.a = a
+let b =function(){
+
+     
+}
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+
     res.send('My first ever api!')
 });
 
